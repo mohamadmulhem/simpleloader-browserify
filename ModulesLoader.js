f2fe562dcdbd3cfea661fa42df57f77c72673997
Mohamad Mulhem <mohmul15@outlook.com>
2018-10-27 21:50:12 +0300

    Create ModulesLoader.js

---
+++ ModulesLoader.js
@@ -0,0 +1,27 @@
+var ML = {
+    objects: []
+    //get instance
+    gI: function (objectClassName) {
+            try {
+                if (!this.objects[objectClassName])
+                {
+                    switch (objectClassName)
+                    {
+                        case 'SampleModule1':
+                            this.objects[objectClassName] = require('./SampleModule1.js');
+                            break;
+						case 'SampleModule2':
+							this.objects[objectClassName] = require('./SampleModule2.js');
+							break;
+                    }
+                }
+                return this.objects[objectClassName];
+
+            } catch (err)
+            {
+                throw new Error(err);
+                return;
+            }
+    }
+}
+module.exports = ML;
